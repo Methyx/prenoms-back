a7b8b1c812bc1db04eae3eed12d404ea78a1ba58
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2023-01-05 09:13:20 +0100

    using aggregate from MongoDB

--- routes/top.js
+++ routes/top.js
@@ -3,21 +3,40 @@ const router = express.Router();
 
 const Prenom = require("../models/Prenom");
 
+// -----------------------------------------
+// Get the list of firstname sort by number
+// -----------------------------------------
+//
+// Query        Exemple                  Require
+//
+// years        2010
+//              or 2010-2015                No
+//
+// gender       M or F                      No
+//
+// dptCode      75                          No
+//
 router.get("/top", async (req, res) => {
   try {
     const { years, gender, dptCode } = req.query;
-    const searchFilters = {};
+    const searchFilters = {
+      preusuel: { $ne: "_PRENOMS_RARES" },
+    };
+
     if (years) {
       const date = years.split("-");
       if (date.length === 1) {
         if (Number(date[0])) {
-          searchFilters.annais = date[0];
+          searchFilters.annais = Number(date[0]);
         } else {
           return res.status(401).json({ message: "bad year" });
         }
       } else {
         if (Number(date[0]) && Number(date[1])) {
-          searchFilters.annais = { $gte: date[0], $lte: date[1] };
+          searchFilters.annais = {
+            $gte: Number(date[0]),
+            $lte: Number(date[1]),
+          };
         } else {
           return res.status(401).json({ message: "bad years" });
         }
@@ -29,36 +48,30 @@ router.get("/top", async (req, res) => {
     } else if (gender === "F") {
       searchFilters.sexe = 2;
     }
+
     if (dptCode && Number(dptCode)) {
-      searchFilters.dpt = dptCode;
+      searchFilters.dpt = Number(dptCode);
     }
-    const results = await Prenom.find(searchFilters).sort("preusuel");
-    const response = [];
-    let name = "";
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].preusuel !== "_PRENOMS_RARES") {
-        if (results[i].preusuel !== name) {
-          name = results[i].preusuel;
-          response.push({
-            name: name,
-            count: Number(results[i].nombre),
-          });
-        } else {
-          response[response.length - 1].count += Number(results[i].nombre);
-        }
-      }
-    }
-    res.json(
-      response.sort((a, b) => {
-        if (a.count < b.count) {
-          return 1;
-        }
-        if (a.count > b.count) {
-          return -1;
-        }
-        return 0;
-      })
-    );
+
+    const results = await Prenom.aggregate([
+      {
+        $match: searchFilters,
+      },
+      {
+        $group: {
+          _id: "$preusuel",
+          total: {
+            $sum: "$nombre",
+          },
+        },
+      },
+      {
+        $sort: {
+          total: -1,
+        },
+      },
+    ]);
+    res.json(results);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
